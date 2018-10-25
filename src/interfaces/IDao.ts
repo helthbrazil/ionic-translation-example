c60778028673548a73ede921ba2db609a3fa1435
Hebert Ferreira <hebertalvesferreira@gmail.com>
2018-10-25 10:13:35 -0300

    remove all

--- src/interfaces/IDao.ts
+++
@@ -1,7 +0,0 @@
-export interface IDao<E> {
-    save(obj: E, callbackSucess, callbackError);
-    update(obj: E, callbackSucess, callbackError);
-    delete(id: number, callbackSucess, callbackError);
-    find(id: number, callbackSucess, callbackError);
-    findAll(callbackSucess, callbackError);
-}
